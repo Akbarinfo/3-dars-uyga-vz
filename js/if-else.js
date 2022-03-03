2415d4d41ba6eaebbb8f0dc46674db758cbb0b69
akbarinfo <girtbezbet@gmail.com>
2022-03-03 08:52:04 +0500

    update

---
+++ js/if-else.js
@@ -0,0 +1,186 @@
+// IF 2
+
+// let ason = 50;
+// let bson = 89;
+// let cson = 1;
+
+// if (ason < bson && bson < cson) {
+//  console.log(bson);
+// } else if (bson < ason && ason < cson) {
+//   console.log(ason);
+// }else if (cson < ason && cson < bson) {
+//   console.log(cson);
+// } else  {
+//   console.log('hammasi teng')
+// }
+
+
+// IF 3
+
+// let ason = 10;
+// let bson = 10;
+
+// if (ason == bson) {
+//   console.log(0);
+// } else if (ason < bson) {
+//   console.log(bson);
+// } else if (ason > bson) {
+//   console.log(ason);
+// }
+
+
+// IF 5
+
+// let ason = 0.5;
+
+// if (ason > 0) {
+//   console.log(2 * Math.sin(ason));
+// } else if (ason < 0) {
+//   console.log(ason - 6);
+// }
+
+
+// IF 6
+
+// let ason = 6;
+
+// if (ason < -2 || ason > 2) {
+//   console.log(2 * ason);
+// } else {
+//   console.log(-3 * ason);
+// }
+
+
+// IF 8
+
+// let ason = 7;
+// let bson = 4;
+// let cson = 2;
+
+// if (ason + bson > cson && bson + cson > ason && ason + cson > bson) {
+//   console.log('Yes')
+// } else {
+//   console.log('No')
+// }
+
+
+// IF 9
+
+// let ason = 55;
+
+// if (ason < 0) {
+//   console.log('Freezing');
+// } else if (ason > 1 && ason < 10) {
+//   console.log('Very Cold');
+// } else if (ason > 11 && ason < 20) {
+//   console.log('Cold');
+// } else if (ason > 21 && ason < 31) {
+//   console.log('Normal');
+// } else if (ason > 31 && ason < 40) {
+//   console.log('Hot');
+// } else if (ason > 40) {
+//   console.log('Very Hot');
+// }
+
+
+// IF 10
+
+// let ason = -10;
+
+// if (ason < 0 && ason % 2 == 0) {
+//   console.log('Manfiy Juft son');
+// } else if (ason < 0 && ason % 2 != 0) {
+//   console.log('Manfiy Toq son');
+// } else if (ason > 0 && ason % 2 == 0) {
+//   console.log('Musbat Juft son');
+// } else if (ason < 0 && ason % 2 != 0) {
+//   console.log('Musbat Toq son');
+// } else if (ason == 0) {
+//   console.log('Son 0 ga teng');
+// }
+
+
+// IF 11
+
+// let ason = 978;
+
+// if (ason > 10 && ason < 99 && ason % 2 != 0) {
+//   console.log('Ikki xonali toq son')
+// }
+// else if (ason > 10 && ason < 99 && ason % 2 == 0) {
+//   console.log('Ikki xonali juft son')
+// }
+// else if (ason > 100 && ason < 999 && ason % 2 != 0) {
+//   console.log('Uch xonali Toq son')
+// }
+// else if (ason > 100 && ason < 999 && ason % 2 == 0) {
+//   console.log('Uch xonali juft son')
+// }
+
+
+// IF 12
+
+// let ason = 0;
+
+// if (ason == 1) {
+//   console.log(ason * ason);
+// }
+// else if (ason == 2) {
+//   console.log(ason * ason);
+// }
+// else if (ason == 0) {
+//   console.log(ason * ason);
+// }
+
+// IF 13
+
+// let sum = 30;
+// let t = true;
+
+// if (sum >= 20 && true == t) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+
+// IF 14
+
+
+// let ason = 99;
+
+// let bson = ason % 10;
+// let cson = (ason - bson) / 10;
+// let dson = bson * 10 + cson;
+
+// if (ason == dson) {
+//   console.log(true);
+// }
+// else
+//   console.log(false);
+
+
+// IF 15
+
+// let ason = 456;
+
+// if (ason < 10) {
+//   console.log('Bir xonali');
+// }
+// else if (ason >= 10 &&  ason <= 99) {
+//   console.log('Ikki xonali');
+// }
+// else if (ason >= 100 &&  ason <= 999) {
+//   console.log('Uch xonali');
+// }
+// else if (ason >= 1000 &&  ason <= 9999) {
+//   console.log("To'rt xonali");
+// }
+// else if (ason >= 10000 &&  ason <= 99999) {
+//   console.log('Besh xonali');
+// }
+
+// IF 16
+
+
---
+++ js/loop.js
@@ -0,0 +1,214 @@
+// loop 1
+
+// let n = 10;
+// let k = 5;
+
+// for(i = 0; i <= n; i++) {
+//   console.log(k);
+// }
+
+
+// FOOR 2
+
+// let a = 9;
+// let b = 2;
+// let count = 0;
+
+// for(i = b; i <= a; i++){
+//   if (i > 0) {
+//     count++;
+//   }
+//   console.log(i);
+// }
+//   console.log(`${count}ta butun son `);
+
+
+// FOOR 3
+
+// let a = 9;
+// let b = 2;
+// let count = 0;
+
+// for(i = a; i >= b; i--){
+//   if (i > 0) {
+//     count++;
+//   }
+//   console.log(i);
+// }
+//   console.log(`${count}ta butun son `);
+
+// FOOR 4
+
+// let kg = 10;
+// let pirce = 4250;
+
+// for(i = 1 ; i <= kg; i++) {
+//   let b = i * pirce;
+//   console.log(`${i}kg konfetni narxi ${b} so'm`);
+// }
+
+
+// FOOR 5
+
+// let kg = 1;
+// let pirce = 4250;
+
+// for(i = 0.1; i <= kg; i += 0.1) {
+//   let b = i * pirce;
+//   console.log(` ${i.toFixed(1)}kg konfetni nari ${b.toFixed(1)}so'm`);
+// }
+
+
+// FOOR 6
+
+// let kg = 2;
+// let pirce = 4250;
+
+// for(i = 1; i <= kg; i += 0.1) {
+//   let b = i * pirce;
+//   console.log(` ${i.toFixed(1)}kg konfetni nari ${b.toFixed(1)}so'm`);
+// }
+
+
+// FOOR 7
+
+// let a = 1;
+// let b = 5;
+// let c = 0;
+// for(i = a; i <= b; i++) {
+//   c += i;
+// }
+// console.log(c);
+
+
+// FOOR 8
+
+// let a = 1;
+// let b = 5;
+// let c = 1;
+// for(i = a; i <= b; i++) {
+//   c *= i;
+// }
+// console.log(c);
+
+
+// FOOR 9
+
+// let a = 1;
+// let b = 5;
+// let c = 0
+// for(i = a; i <= b; i++) {
+//   c += i * i;
+// }
+// console.log(c);
+
+// FOOR 10
+
+
+
+// FOOR 11
+
+// let a = 6;
+// let b = 0;
+
+// for(i = 0; i < a; i++) {
+//   b += (a + i) ** 2;
+// }
+
+// // console.log(b);
+
+// // FOOR 12
+
+// // let a = 2;
+// // let count = 1;
+// // for(i = 1.1; i < a; i+=0.1) {
+// //   count *=i;
+// // }
+
+// // console.log(count.toFixed(1));
+
+
+// // console.log(count.toFixed(1));
+
+// // FOOR 12
+
+// let son = 2
+// let toq = 0
+// let juft = 0
+// for(let i = 1.1; i <= son; i += 0.2) {
+//   toq += i
+//   juft += (i + 0.1)
+// }
+
+// console.log(toq - juft)
+
+// FOOR 14
+// let n = 4
+// let num = 0
+// for(let i = 1; i <= n*2 - 1; i+=2) {
+//   num += i
+//   console.log(num)
+// }
+
+
+// FOOR 15
+
+// let n = 5;
+// let s = 2;
+// let sum = 1;
+// for(let i = 1; i <= s; i++) {
+//   sum *= n;
+// }
+
+// console.log(sum);
+
+
+// FOOR 16
+
+// let n = 8;
+// let a = 2;
+// for(let i = 1; i <= n; i++) {
+//   console.log(a ** i);
+// }
+
+// FOOR 17
+
+// let n = 5;
+// let a = 2;
+// let sum = 0;
+// for(let i = 1; i <= n; i++) {
+//   console.log(a**i);
+//   sum += a**i;
+// }
+// console.log(`Darajalar yig'indisi - ${sum}`);
+
+// FOOR 18
+
+// let n = 5;
+// let a = 2;
+// let sum = 0;
+// for(let i = 1; i < n; i++) {
+//   console.log(a**i);
+//   sum += a**i;
+// }
+// console.log(`Darajalar yig'indisi - ${sum}`);
+
+// FOOR 19
+
+// let a = 5;
+// let count = 1;
+// for(i = 1; i <= a; i++) {
+//   count *= i;
+// }
+
+// console.log(count);
+
+// FOOR 20
+
+// let n = 5;
+// let sum = 1;
+// let count = 0;
+// for(let i = 1; i <= n; i++) {
+//   count += sum *= i;
+// }
+// console.log(count);
